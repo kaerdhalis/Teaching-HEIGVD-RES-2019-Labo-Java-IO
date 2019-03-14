de6a2534ee642e22295f2dd0cbebb3f790dc93b3
kaerdhalis <benjamin.leguillou@heig-vd.ch>
2019-03-14 11:02:15 +0100

    DFSFileExplorer fonctionne

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -15,8 +15,25 @@ import java.io.File;
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  public void explore(File rootDirectory, IFileVisitor visitor) {
+
+
+    File currentFile = rootDirectory;
+    visitor.visit(currentFile);
+
+    if(currentFile.isDirectory()){
+
+        for( File f : currentFile.listFiles()) {
+            explore(f, visitor);
+        }
+
+    }
+    else if(currentFile.isFile()) {
+
+        visitor.visit(currentFile);
+        return;
+    }
+
   }
 
 }
