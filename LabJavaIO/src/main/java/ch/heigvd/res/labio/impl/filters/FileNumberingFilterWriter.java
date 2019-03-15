99229e3d37b3540a9b5bfb681f233ff2d9d196da
kaerdhalis <benjamin.leguillou@heig-vd.ch>
2019-03-15 19:35:40 +0100

    FileNumbering fonctionne

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -17,25 +19,113 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
+
+  private int count;
+  private boolean flag_debut;
+  private boolean flag_fin;
+  private String line;
+
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    count = 1;
+    flag_debut = true;
+    flag_fin = false;
+    line = count+"\t";
+    count++;
+
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    str=str.substring(off,off+ len);
+
+
+
+    String[] newLines = new String[2];
+
+    newLines = Utils.getNextLine(str);
+
+    while(!newLines[0].equals("")){
+
+      line = line.concat(newLines[0]+count+"\t");
+      count++;
+
+      newLines = Utils.getNextLine(newLines[1]);
+
+    }
+
+   line= line.concat(newLines[1]);
+
+
+
+    super.write(line,0,line.length());
+    line = "";
+
+
+
+
+
+
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    String str = new String(cbuf);
+    this.write(str,off,len);
+
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    if(flag_debut){
+
+      super.write('1');
+      super.write('\t');
+
+      flag_debut=false;
+
+    }
+
+
+
+    if(flag_fin){
+
+
+
+      if(c == '\n') {
+
+        super.write(c);
+        super.write((char)(count + '0'));
+        super.write('\t');
+        count++;
+      }
+      else{
+        super.write((char)(count + '0'));
+        super.write('\t');
+          count++;
+        super.write(c);
+        }
+      flag_fin=false;
+      return;
+      }
+
+    if(c == '\r'){
+      flag_fin = true;
+    }
+    if(c =='\n'){
+
+      super. write(c);
+      super.write((char)(count + '0'));
+      super.write('\t');
+      count++;
+      return;
+  }
+    super. write(c);
   }
 
 }
